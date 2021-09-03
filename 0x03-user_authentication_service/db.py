25564842b4fb2622e27aecaf95e035ee8d66c340
KhaoulaDER <58639852+KhaoulaDER@users.noreply.github.com>
2021-09-03 12:04:22 +0100

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -52,8 +52,8 @@ class DB:
         user = self.find_user_by(id=user_id)
 
         for key, value in kwargs.items():
-            if hasattr(user, key):
-                setattr(user, key, value)
+            if not hasattr(user, key):
+                raise ValueError                
             else:
-                raise ValueError
+                setattr(user, key, value)                
         self._session.commit()
