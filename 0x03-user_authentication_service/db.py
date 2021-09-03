ffd54178c296811573b93f194d2d9232d70b3409
KhaoulaDER <58639852+KhaoulaDER@users.noreply.github.com>
2021-09-03 17:17:00 +0100

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-"""
-DB model
-0x08. User authentication service
-holbertonschool-web_back_end
+""" DB module to save and update databse
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -39,25 +37,15 @@ class DB:
         return new_user
 
     def find_user_by(self, **kwargs) -> User:
-        """
-        Description: Find a user by keyword argument
-        Args:
-            keyword argument([dict]): [user Input]
-        Return:
-            user instance if user exist or raise an Error
-        """
+        """ Returns first row found in users table based on keyword args """
 
         try:
-            dec = list(kwargs.items())
-            user = self._session.query(User).filter(
-                getattr(User, dec[0][0]) == dec[0][1]).first()
-        except InvalidRequestError:
+            record = self._session.query(User).filter_by(**kwargs).first()
+        except TypeError:
             raise InvalidRequestError
-        except AttributeError:
+        if record is None:
             raise NoResultFound
-        if user is None:
-            raise NoResultFound
-        return user
+        return record
 
     def update_user(self, user_id: int, **kwargs) -> None:
         """ Finds user record and updates attributes """
