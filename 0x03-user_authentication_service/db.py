bc3ec16e71ec5ed0278f754772a2de5d5e654238
KhaoulaDER <58639852+KhaoulaDER@users.noreply.github.com>
2021-09-03 10:26:13 +0100

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -48,7 +48,10 @@ class DB:
         return record
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """ Finds user record and updates attributes """
+        """ Finds user record and updates attributes 
+         Args:
+            user_id ([str]): [description]
+        """
         user_record = self.find_user_by(id=user_id)
 
         for key, value in kwargs.items():
